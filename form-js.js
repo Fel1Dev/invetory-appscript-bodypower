d93f36f520854d193e30ed45962cf138107db62c
Andres Felipe Torres <andrestor2@gmail.com>
2023-01-20 16:37:03 -0500

    Added logic to handle dataLists

--- form-js.js
+++ form-js.js
@@ -3,35 +3,55 @@ document.getElementById('clear-fields').addEventListener("click", clearFields);
 document.getElementById('create-record').addEventListener("click", createRecord);
 window.addEventListener("load", startUpForm);
 
-    function startUpForm() {
-      //set currentDate with timezone
-      document.getElementById('record-date').value = getLocalDateAsValue();
+function startUpForm() {
+  //set currentDate with timezone
+  document.getElementById('record-date').value = getLocalDateAsValue();
 
-      //Initialize lists
-      loadItemData();
-    }
+  //Initialize lists
+  loadItemData();
+  loadUserData();
+}
 
-    function getLocalDateAsValue() {
-      let local = new Date();
-      local.setMinutes(local.getMinutes() - local.getTimezoneOffset());
-      return local.toJSON().slice(0,10);
-    }
+function getLocalDateAsValue() {
+  let local = new Date();
+  local.setMinutes(local.getMinutes() - local.getTimezoneOffset());
+  return local.toJSON().slice(0,10);
+}
 
-    function loadItems( itemsData ) {
-      console.log('loadItems');
-      const itemListInput = document.getElementById('item-list-options');
-      itemsData.forEach(item => {
-        const option = document.createElement('option');
-        option.value = item[1];
-        option.text = item[1] + ", " + item[3];
-        itemListInput.appendChild(option);
-      });
-    }
+function loadItemList( itemsData ) {
+  console.log('loadItems');
+  const itemListInput = document.getElementById('item-list-options');
+  itemsData.forEach(item => {
+    const option = document.createElement('option');
+    option.value = item[1];
+    option.text = item[1] + ", " + item[3];
+    itemListInput.appendChild(option);
+  });
+}
 
-    function loadItemData() {
-      console.log('DataList ItemData');
-      google.script.run.withSuccessHandler( loadItems ).getItemsData();
-    }
+function loadItemData() {
+  console.log('DataList ItemData');
+  google.script.run.withSuccessHandler( loadItemList ).getItemsData();
+}
+
+function loadUserList( usersData ) {
+  console.log('loadItems');
+  console.table(usersData);
+  const usersListInput = document.getElementById('user-list-options');
+  const unitsListInput = document.getElementById('unit-list-options');
+  const newOutTypesListInput = document.getElementById('new-output-list-options');
+  const outOutTypesListInput = document.getElementById('output-list-options');
+  usersData.forEach(user => {
+    const option = document.createElement('option');
+    option.value = user[1];
+    option.text = user[1];
+    usersListInput.appendChild(option);
+  });
+}
+
+function loadUserData() {
+  google.script.run.withSuccessHandler( loadUserList ).getUsersData();
+}
 
 
 function clearFields() {
@@ -47,7 +67,7 @@ function clearFields() {
     // General fields
     document.getElementById("item-list").value = "";
     document.getElementById("current-stock").value = "";
-    document.getElementById("units").value = "";
+    document.getElementById("unit-list").value = "";
     // New Stock
     document.getElementById("new-quantity").value = "";
     document.getElementById("final-stock-new").value = "";
@@ -55,7 +75,7 @@ function clearFields() {
     document.getElementById("new-stock-invoice").value = "";
     document.getElementById("new-stock-amount").value = "";
 
-    document.getElementById("new-output-type").value = "";
+    document.getElementById("new-output-type-list").value = "";
     document.getElementById("new-output-desc").value = "";
     
     // Input
@@ -65,9 +85,9 @@ function clearFields() {
     document.getElementById("final-stock-input").value = "";
     
     // Output
-    document.getElementById("output-type").value = "";
     document.getElementById("out-quantity").value = "";
     document.getElementById("final-stock-out").value = "";
+    document.getElementById("output-type-list").value = "";
     document.getElementById("output-description").value = "";
 
     startUpForm();
