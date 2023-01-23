1db1adb8205435cf3774ee413015d22d34dd10b1
Andres Felipe Torres <andrestor2@gmail.com>
2023-01-23 01:33:38 -0500

    Added logic to hide or show fields according the new stock value. Deleted unit list field

--- form-js.js
+++ form-js.js
@@ -1,8 +1,98 @@
 
+/* Field definition */
+const REACORD_DATE_ID = 'record-date';
+const USER_LIST_ID = 'user-list';
+const ITEM_LIST_ID = 'item-list';
+const CURRENT_STOCK_ID = 'current-stock';
+const NEW_QUANTITY_ID = 'new-quantity';
+
+const OUTPUT_TYPE_BAJA = 'BAJA';
+
+/* Initial events */
+window.addEventListener("load", startUpForm);
 document.getElementById('clear-fields').addEventListener('click', clearFields);
 document.getElementById('create-record').addEventListener('click', createRecord);
-document.getElementById('new-quantity').addEventListener('change', processNewStock)
-window.addEventListener("load", startUpForm);
+document.getElementById(NEW_QUANTITY_ID).addEventListener('change', processNewStock);
+document.getElementById(ITEM_LIST_ID).addEventListener('change', getCurrentStock );
+document.getElementById('new-output-type-list').addEventListener('change', processOutputType);
+
+document.getElementById('new-stock-tab').addEventListener('click', clickOnNewStock);
+document.getElementById('input-tab').addEventListener('click', clickOnInput);
+document.getElementById('output-tab').addEventListener('click', clickOnOutput);
+
+function clearFields() {
+  let keepDate = document.getElementById("keep-date").checked;
+  let keepUserList = document.getElementById("keep-user-list").checked;
+
+  if(!keepDate) {
+      document.getElementById(REACORD_DATE_ID).value = "";
+  }
+  if(!keepUserList) {
+      document.getElementById(USER_LIST_ID).value = "";
+  }       
+  // General fields
+  document.getElementById(ITEM_LIST_ID).value = "";
+  document.getElementById(CURRENT_STOCK_ID).value = "";
+  // document.getElementById("unit-list").value = "";
+  
+  // New Stock
+  clearNewStockFields();
+  
+  // Input
+  clearInputFields();
+  
+  // Output
+  clearOutputFields();
+}
+
+function clearNewStockFields() {
+  // New Stock
+  document.getElementById(NEW_QUANTITY_ID).value = "";
+  document.getElementById("final-stock-new").value = "";
+
+  document.getElementById("new-stock-invoice").value = "";
+  document.getElementById("new-stock-amount").value = "";
+
+  document.getElementById("new-output-type-list").value = "";
+  document.getElementById("new-output-desc").value = "";
+
+  hideStockFields();
+}
+
+function clearInputFields() {
+  document.getElementById("invoice").value = "";
+  document.getElementById("amount").value = "";
+  document.getElementById("quantity").value = "";
+  document.getElementById("final-stock-input").value = "";
+}
+
+function clearOutputFields() {
+  document.getElementById("out-quantity").value = "";
+  document.getElementById("final-stock-out").value = "";
+  document.getElementById("output-type-list").value = "";
+  document.getElementById("output-description").value = "";
+}
+
+function hideStockFields() {
+  blockNewQuantity();
+  hideStockInputFields();
+  hideStockOutputFields();
+}
+
+function clickOnInput() {
+  clearNewStockFields();
+  clearOutputFields();
+}
+
+function clickOnOutput() {
+  clearNewStockFields();
+  clearInputFields();
+}
+
+function clickOnNewStock() {
+  clearOutputFields();
+  clearInputFields();
+}
 
 function startUpForm() {
   initCurrDateField();
@@ -27,10 +117,9 @@ function createItemsOptions( itemsData ) {
   itemsData.forEach(item => {
     const option = document.createElement('option');
     option.value = item[1];
-    option.text = item[1] + ", " + item[3];
+    option.text =  `Cantidad actual: ${item[7]} ${item[4]}`;
     itemListInput.appendChild(option);
   });
-  addListenerItemChange();
 }
 
 function loadItemsData() {
@@ -39,11 +128,11 @@ function loadItemsData() {
 
 function createSimpleListsOptions( allListsData ) {
   const usersListInput = document.getElementById('user-list-options');
-  const unitsListInput = document.getElementById('unit-list-options');
+  // const unitsListInput = document.getElementById('unit-list-options');
   const newOutTypesListInput = document.getElementById('new-output-list-options');
   const outOutTypesListInput = document.getElementById('output-list-options');
   allListsData.forEach(dataRow => {
-    createListOption(unitsListInput, dataRow[0]);
+    // createListOption(unitsListInput, dataRow[0]);
     createListOption(usersListInput, dataRow[1]);
     createListOption(newOutTypesListInput, dataRow[3]);
     createListOption(outOutTypesListInput, dataRow[3]);
@@ -63,46 +152,6 @@ function loadSimpleListsData() {
   google.script.run.withSuccessHandler( createSimpleListsOptions ).getSimpleListsData();
 }
 
-
-function clearFields() {
-    let keepDate = document.getElementById("keep-date").checked;
-    let keepUserList = document.getElementById("keep-user-list").checked;
-
-    if(!keepDate) {
-        document.getElementById("record-date").value = "";
-    }
-    if(!keepUserList) {
-        document.getElementById("user-list").value = "";
-    }       
-    // General fields
-    document.getElementById("item-list").value = "";
-    document.getElementById("current-stock").value = "";
-    document.getElementById("unit-list").value = "";
-    // New Stock
-    document.getElementById("new-quantity").value = "";
-    document.getElementById("final-stock-new").value = "";
-
-    document.getElementById("new-stock-invoice").value = "";
-    document.getElementById("new-stock-amount").value = "";
-
-    document.getElementById("new-output-type-list").value = "";
-    document.getElementById("new-output-desc").value = "";
-    
-    // Input
-    document.getElementById("invoice").value = "";
-    document.getElementById("amount").value = "";
-    document.getElementById("quantity").value = "";
-    document.getElementById("final-stock-input").value = "";
-    
-    // Output
-    document.getElementById("out-quantity").value = "";
-    document.getElementById("final-stock-out").value = "";
-    document.getElementById("output-type-list").value = "";
-    document.getElementById("output-description").value = "";
-
-    initCurrDateField();
-}
-
 function createRecord() {
   //read fields
   console.log('create record');
@@ -117,20 +166,22 @@ function startLoadingScreen() {
   document.getElementById('loading').classList.remove('hide');
 }
 
-function addListenerItemChange() {
-  document.getElementById('item-list').addEventListener('change', getCurrentStock );
-}
 
-function getCurrentStock() {
+function getCurrentStock(event) {
   startLoadingScreen();
-  const itemName = document.getElementById('item-list').value;
-  document.getElementById('new-quantity').value = '';
+  const itemName = event.target.value
+  console.log('value: ' + event.target.value);
+  console.log('text: ' + event.target.text);
+  console.log(event.target.text);
+  document.getElementById(NEW_QUANTITY_ID).value = '';
   //Call backed function to get only one row
   if(itemName) {
     searchItemData(itemName)
   } else {
     updateCurrentStock('');
-    disableNewQuantity();
+    blockNewQuantity();
+    hideStockInputFields();
+    hideStockOutputFields();
   }
 }
 
@@ -140,8 +191,8 @@ function searchItemData( name ) {
 
 function processItemData( itemData ) {
   if(itemData){
-    enableNewQuantity();
-    updateCurrentStock(parseInt(itemData));
+    unBlockNewQuantity();
+    updateCurrentStock(parseInt(itemData));  
   }
 }
 
@@ -150,40 +201,80 @@ function processEmptyItemData() {
 }
 
 function updateCurrentStock(value) {
-  document.getElementById('current-stock').value = value;
+  document.getElementById(CURRENT_STOCK_ID).value = value;
   stopLoadingScreen();
 }
 
-function disableNewQuantity() {
-  document.getElementById('new-quantity').disabled = true;
+function blockNewQuantity() {
+  document.getElementById(NEW_QUANTITY_ID).disabled = true;
 }
 
-function enableNewQuantity() {  
-  document.getElementById('new-quantity').disabled = false;
+function unBlockNewQuantity() {  
+  document.getElementById(NEW_QUANTITY_ID).disabled = false;
 }
 
 function getNewQuantityValue() {
-  return document.getElementById('new-quantity').value;
+  return document.getElementById(NEW_QUANTITY_ID).value;
 }
 
 function getStockValue() {
-  return document.getElementById('current-stock').value;
+  return document.getElementById(CURRENT_STOCK_ID).value;
 }
 
 function processNewStock() {
   let newQuantity = getNewQuantityValue();
   if(newQuantity) {
     let currentStock = getStockValue();
-    //Check the logic in spredsheet to do this action.
+    let difference = newQuantity - currentStock;
+    updateDifferenceValue(difference);
     //TODO
     //  Check difference
     //  Enable Input or Output fields according difference value
-    //  If out type
-   //     Enable Detail field when outputType is 'Baja'
+    if(difference > 0) {
+      showStockInputFields();
+      hideStockOutputFields();
+    } else {
+      showStockOutputFields();
+      hideStockInputFields();
+    }
   }
+}
+
+function showStockInputFields() {
+  document.getElementById('new-stock-input-fields').classList.remove('hidden');
+}
+
+function hideStockInputFields() {
+  document.getElementById('new-stock-input-fields').classList.add('hidden');   
+}
+
+function showStockOutputFields() {
+  document.getElementById('new-stock-output-fields').classList.remove('hidden');   
+}
+
+function hideStockOutputFields() {
+  document.getElementById('new-stock-output-fields').classList.add('hidden');
+  document.getElementById('new-stock-output-detail').classList.add('hidden');
+}
+
+//  If out type
+//     Enable Detail field when outputType is 'Baja'
+function showStockOutputDetail() {
+  document.getElementById('new-stock-output-detail').classList.remove('hidden');
+}
+
+function hideStockOutputDetail() {
+  document.getElementById('new-stock-output-detail').classList.add('hidden');
 }
 
 function updateDifferenceValue(newValue) {
   document.getElementById('final-stock-new').value = newValue;
 }
 
+function processOutputType(event) { 
+  if(event.target.value.toUpperCase() === OUTPUT_TYPE_BAJA) {
+    showStockOutputDetail();
+  } else {
+    hideStockOutputDetail();
+  }
+}
