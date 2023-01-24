81c0575a357a6b1856ddbee6693725cda2bb2cda
Andres Felipe Torres <andrestor2@gmail.com>
2023-01-24 18:47:15 -0500

    Added variable to handle all fields. Changed some field names to improve readability

--- form-controls.js
+++ form-controls.js
@@ -1,10 +1,31 @@
 
 /* Field definition */
-const REACORD_DATE_ID = 'record-date';
+const RECORD_DATE_ID = 'record-date';
 const USER_LIST_ID = 'user-list';
 const ITEM_LIST_ID = 'item-list';
 const CURRENT_STOCK_ID = 'current-stock';
-const NEW_QUANTITY_ID = 'new-quantity';
+
+/* New Stock tab */
+const NEW_STOCK_ID = 'new-stock';
+const DIFFERENCE_ID = 'difference';
+const STOCK_INVOICE_ID = 'stock-invoice';
+const STOCK_AMOUNT_ID = 'stock-amount';
+
+const STOCK_OUTPUT_TYPE_LIST = 'stock-output-type-list';
+const STOCK_OUTPUT_DESC = 'new-output-desc';
+
+/* Input Tab */
+const INVOICE_ID = 'invoice';
+const AMOUNT_ID = 'amount';
+const QUANTITY_ID = 'quantity';
+const FINAL_STOCK_INPUT_ID = 'final-stock-input';
+
+/* Output Tab */
+const OUT_QUANTITY_ID = 'out-quantity';
+const FINAL_STOCK_OUT = 'final-stock-out';
+const OUTPUT_TYPE_LIST = 'output-type-list';
+const OUTPUT_DESC = 'output-description';
+
 
 const OUTPUT_TYPE_BAJA = 'BAJA';
 
@@ -13,7 +34,7 @@ function clearFields() {
     let keepUserList = document.getElementById("keep-user-list").checked;
   
     if(!keepDate) {
-        document.getElementById(REACORD_DATE_ID).value = "";
+        document.getElementById(RECORD_DATE_ID).value = "";
     }
     if(!keepUserList) {
         document.getElementById(USER_LIST_ID).value = "";
@@ -35,70 +56,82 @@ function clearFields() {
   
   function clearNewStockFields() {
     // New Stock
-    document.getElementById(NEW_QUANTITY_ID).value = "";
-    document.getElementById("final-stock-new").value = "";
+    document.getElementById(NEW_STOCK_ID).value = "";
+    document.getElementById(DIFFERENCE_ID).value = "";
   
-    document.getElementById("new-stock-invoice").value = "";
-    document.getElementById("new-stock-amount").value = "";
+    document.getElementById(STOCK_INVOICE_ID).value = "";
+    document.getElementById(STOCK_AMOUNT_ID).value = "";
   
-    document.getElementById("new-output-type-list").value = "";
-    document.getElementById("new-output-desc").value = "";
+    document.getElementById(STOCK_OUTPUT_TYPE_LIST).value = "";
+    document.getElementById(STOCK_OUTPUT_DESC).value = "";
   
     hideStockFields();
   }
   
   function clearInputFields() {
     // Input
-    document.getElementById("invoice").value = "";
-    document.getElementById("amount").value = "";
-    document.getElementById("quantity").value = "";
-    document.getElementById("final-stock-input").value = "";
+    document.getElementById(INVOICE_ID).value = "";
+    document.getElementById(AMOUNT_ID).value = "";
+    document.getElementById(QUANTITY_ID).value = "";
+    document.getElementById(FINAL_STOCK_INPUT_ID).value = "";
   }
   
   function clearOutputFields() {
     // Output
-    document.getElementById("out-quantity").value = "";
-    document.getElementById("final-stock-out").value = "";
-    document.getElementById("output-type-list").value = "";
-    document.getElementById("output-description").value = "";
+    document.getElementById(OUT_QUANTITY_ID).value = "";
+    document.getElementById(FINAL_STOCK_OUT).value = "";
+    document.getElementById(OUTPUT_TYPE_LIST).value = "";
+    document.getElementById(OUTPUT_DESC).value = "";
   }
   
   function showStockInputFields() {
     document.getElementById('new-stock-input-fields').classList.remove('hidden');
-    
-    document.getElementById("new-stock-amount").disabled = false;
+        
+    document.getElementById(STOCK_AMOUNT_ID).disabled = false;
   }
   
   function hideStockInputFields() {
     document.getElementById('new-stock-input-fields').classList.add('hidden');
     
-    document.getElementById("new-stock-amount").disabled = true; 
+    document.getElementById(STOCK_AMOUNT_ID).disabled = true;
   }
   
   function showStockOutputFields() {
     document.getElementById('new-stock-output-fields').classList.remove('hidden');
   
-    document.getElementById("new-output-type-list").disabled = false;  
+    document.getElementById(STOCK_OUTPUT_TYPE_LIST).disabled = false;  
   }
   
   function hideStockOutputFields() {
     document.getElementById('new-stock-output-fields').classList.add('hidden');
     document.getElementById('new-stock-output-detail').classList.add('hidden');
   
-    document.getElementById("new-output-type-list").disabled = true;
-    document.getElementById("new-output-desc").disabled = true;
+    document.getElementById(STOCK_OUTPUT_TYPE_LIST).disabled = true;
+    document.getElementById(STOCK_OUTPUT_DESC).disabled = true;
   }
   
   function showStockOutputDetail() {
     document.getElementById('new-stock-output-detail').classList.remove('hidden');
   
-    document.getElementById("new-output-desc").disabled = false;
+    document.getElementById(STOCK_OUTPUT_DESC).disabled = false;
   }
   
   function hideStockOutputDetail() {
     document.getElementById('new-stock-output-detail').classList.add('hidden');
   
-    document.getElementById("new-output-desc").disabled = true;
+    document.getElementById(STOCK_OUTPUT_DESC).disabled = true;
+  }
+
+  function showStockInputFields() {
+    document.getElementById(INVOICE_ID).disabled = false;
+    document.getElementById(AMOUNT_ID).disabled = false;
+    document.getElementById(QUANTITY_ID).disabled = false;
+  }
+
+  function hideStockInputFields() {
+    document.getElementById(INVOICE_ID).disabled = true;
+    document.getElementById(AMOUNT_ID).disabled = true;
+    document.getElementById(QUANTITY_ID).disabled = true;
   }
 
   function hideStockFields() {
@@ -110,6 +143,7 @@ function clearFields() {
   function clickOnInput() {
     clearNewStockFields();
     clearOutputFields();
+    showStockInputFields();
   }
   
   function clickOnOutput() {
@@ -120,6 +154,11 @@ function clearFields() {
   function clickOnNewStock() {
     clearOutputFields();
     clearInputFields();
+
+    const currStock = document.getElementById(CURRENT_STOCK_ID).value;
+    if(currStock) {
+      unBlockNewStock();
+    }
   }
 
   function stopLoadingScreen() {
@@ -131,10 +170,10 @@ function clearFields() {
   }
 
   function blockNewQuantity() {
-    document.getElementById(NEW_QUANTITY_ID).disabled = true;
+    document.getElementById(NEW_STOCK_ID).disabled = true;
   }
   
-  function unBlockNewQuantity() {  
-    document.getElementById(NEW_QUANTITY_ID).disabled = false;
+  function unBlockNewStock() {  
+    document.getElementById(NEW_STOCK_ID).disabled = false;
   }
   
