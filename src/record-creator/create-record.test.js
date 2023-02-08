d07c72bf4081fbff67dba40cd84683787810fdf0
Andres Felipe Torres <andrestor2@gmail.com>
2023-02-08 18:36:42 -0500

    added unit test scenario of the day

--- src/record-creator/create-record.test.js
+++ src/record-creator/create-record.test.js
@@ -41,4 +41,40 @@ describe('record-creator.html', () => {
     console.log(document.getElementById('record-date').value);
     expect(dateField.value.length).toBeGreaterThan(0);
   });
+
+  test('renders same Fecha and Encargado after checkbox and click on Limpiar', async () => {
+    let { document } = await load(testPath);
+
+    /* fields */
+    const dateField = document.getElementById('record-date');
+    const dateCheckbox = document.getElementById('keep-date');
+    const encargadoField = document.getElementById('user-list');
+    const encargadoCheckbox = document.getElementById('keep-user-list');
+    const productField = document.getElementById('item-list');
+    const currentStockField = document.getElementById('current-stock');
+
+    const limpiarButton = document.getElementById('clear-fields');
+
+    /* static values */
+    const encargadaFer = 'Fernanda Ruiz';
+    const testProduct = 'Sal cocina';
+    const dateValue = dateField.value;
+    
+    /*Active checkbox */
+    dateCheckbox.checked = true;
+    encargadoCheckbox.checked = true;
+
+    /*Assing new values */
+    encargadoField.value = encargadaFer;
+    fireEvent.change(productField, { target: { value: testProduct } })
+
+    /*Check load static value of current stock */
+    expect(currentStockField.value).toBe("10");
+
+    fireEvent.click(limpiarButton);
+
+    expect(dateField.value).toBe(dateValue);
+    expect(encargadoField.value).toBe(encargadaFer);
+    expect(productField.value).not.toBe(testProduct);
+  });
 });
