79c6aa2767c9d127b1df73dc19d5d753678ca324
Andres Felipe Torres <andrestor2@gmail.com>
2023-02-09 17:53:16 -0500

    Added daily unit test

--- src/record-creator/create-record.test.js
+++ src/record-creator/create-record.test.js
@@ -5,6 +5,7 @@ import {
   getByTestId,
   queryByTestId,
 } from '@testing-library/dom';
+import '@testing-library/jest-dom';
 import { JSDOM } from 'jsdom';
 
 const testPath = './src/record-creator/record-creator.html';
@@ -36,9 +37,9 @@ describe('record-creator.html', () => {
 
   test('renders current date when the page load', async () => {
     let { document } = await load(testPath);
+
     const dateField = document.getElementById('record-date');
 
-    console.log(document.getElementById('record-date').value);
     expect(dateField.value.length).toBeGreaterThan(0);
   });
 
@@ -50,6 +51,7 @@ describe('record-creator.html', () => {
     const dateCheckbox = document.getElementById('keep-date');
     const encargadoField = document.getElementById('user-list');
     const encargadoCheckbox = document.getElementById('keep-user-list');
+
     const productField = document.getElementById('item-list');
     const currentStockField = document.getElementById('current-stock');
 
@@ -59,22 +61,50 @@ describe('record-creator.html', () => {
     const encargadaFer = 'Fernanda Ruiz';
     const testProduct = 'Sal cocina';
     const dateValue = dateField.value;
-    
+
     /*Active checkbox */
     dateCheckbox.checked = true;
     encargadoCheckbox.checked = true;
 
     /*Assing new values */
     encargadoField.value = encargadaFer;
-    fireEvent.change(productField, { target: { value: testProduct } })
+    fireEvent.change(productField, { target: { value: testProduct } });
 
     /*Check load static value of current stock */
-    expect(currentStockField.value).toBe("10");
+    expect(currentStockField.value).toBe('10');
 
     fireEvent.click(limpiarButton);
 
     expect(dateField.value).toBe(dateValue);
     expect(encargadoField.value).toBe(encargadaFer);
     expect(productField.value).not.toBe(testProduct);
+  });
+
+  test('renders difference value', async () => {
+    let { document } = await load(testPath);
+
+    const productField = document.getElementById('item-list');
+    const currentStockField = document.getElementById('current-stock');
+    const newStock = document.getElementById('new-stock');
+    const differenceField = document.getElementById('difference');
+    const outputFields = document.getElementById('new-stock-output-fields');
+    const sotckOutType = document.getElementById('stock-output-type-list');
+    const stockOutputDesc = document.getElementById('new-output-desc');
+
+    const testProduct = 'Sal cocina';
+    const currentStockInput = 5;
+    const BAJA_VALUE = 'BAJA';
+
+    expect(stockOutputDesc).toBeDisabled();
+
+    fireEvent.change(productField, { target: { value: testProduct } });
+    expect(currentStockField.value).toBe('10');
+
+    fireEvent.input(newStock, { target: { value: currentStockInput } });
+    expect(differenceField.value).toBe('-5');
+    expect(outputFields).not.toBeDisabled();
+
+    fireEvent.input(sotckOutType, { target: { value: BAJA_VALUE } });
+    expect(stockOutputDesc).not.toBeDisabled();
   });
 });
