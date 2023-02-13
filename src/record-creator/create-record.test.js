ac653a9e47b575f025e7d55b8244dfa1ba875923
Andres Felipe Torres <andrestor2@gmail.com>
2023-02-13 08:07:20 -0500

    Added UI test.

--- src/record-creator/create-record.test.js
+++ src/record-creator/create-record.test.js
@@ -10,7 +10,7 @@ import { JSDOM } from 'jsdom';
 
 const testPath = './src/record-creator/record-creator.html';
 
-describe('record-creator.html', () => {
+describe('Record Creator UI', () => {
   async function load(file) {
     let dom = await JSDOM.fromFile(file, {
       runScripts: 'dangerously',
@@ -27,32 +27,43 @@ describe('record-creator.html', () => {
     });
   }
 
-  function getStockFields() {
+  const getCommonFields = (document) => {
+    return {
+      dateField: document.getElementById('record-date'),
+      dateCheckbox: document.getElementById('keep-date'),
+      encargadoField: document.getElementById('user-list'),
+      encargadoCheckbox: document.getElementById('keep-user-list'),
+      productField: document.getElementById('item-list'),
+      currentStockField: document.getElementById('current-stock'),
+    };
+  };
+
+  const getStockFields = (document) => {
     return {
       newStock: document.getElementById('new-stock'),
       differenceField: document.getElementById('difference'),
       outputFields: document.getElementById('new-stock-output-fields'),
       sotckOutType: document.getElementById('stock-output-type-list'),
       stockOutputDesc: document.getElementById('new-output-desc'),
+      inputFields: document.getElementById('new-stock-input-fields'),
       stockInvoice: document.getElementById('stock-invoice'),
       stockAmount: document.getElementById('stock-amount'),
     };
-  }
+  };
 
-  function getInputFields() {
+  const getInputFields = (document) => {
     return {
       invoice: document.getElementById('invoice'),
       amount: document.getElementById('amount'),
       quantity: document.getElementById('quantity'),
       finalStockInput: document.getElementById('final-stock-input'),
     };
-  }
+  };
 
-  function getOutputFields() {
+  function getOutputFields(document) {
     return {
       outQuantity: document.getElementById('out-quantity'),
       finalStockOut: document.getElementById('final-stock-out'),
-      quantity: document.getElementById('quantity'),
       outputTypeList: document.getElementById('output-type-list'),
       outputDescription: document.getElementById('output-description'),
     };
@@ -78,37 +89,31 @@ describe('record-creator.html', () => {
     let { document } = await load(testPath);
 
     /* fields */
-    const dateField = document.getElementById('record-date');
-    const dateCheckbox = document.getElementById('keep-date');
-    const encargadoField = document.getElementById('user-list');
-    const encargadoCheckbox = document.getElementById('keep-user-list');
-
-    const productField = document.getElementById('item-list');
-    const currentStockField = document.getElementById('current-stock');
+    const commonFields = getCommonFields(document);
 
     const limpiarButton = document.getElementById('clear-fields');
 
     /* static values */
     const encargadaFer = 'Fernanda Ruiz';
     const testProduct = 'Sal cocina';
-    const dateValue = dateField.value;
+    const dateValue = commonFields.dateField.value;
 
     /*Active checkbox */
-    dateCheckbox.checked = true;
-    encargadoCheckbox.checked = true;
+    commonFields.dateCheckbox.checked = true;
+    commonFields.encargadoCheckbox.checked = true;
 
     /*Assing new values */
-    encargadoField.value = encargadaFer;
-    fireEvent.change(productField, { target: { value: testProduct } });
+    commonFields.encargadoField.value = encargadaFer;
+    fireEvent.change(commonFields.productField, { target: { value: testProduct } });
 
     /*Check load static value of current stock */
-    expect(currentStockField.value).toBe('10');
+    expect(commonFields.currentStockField.value).toBe('10');
 
     fireEvent.click(limpiarButton);
 
-    expect(dateField.value).toBe(dateValue);
-    expect(encargadoField.value).toBe(encargadaFer);
-    expect(productField.value).not.toBe(testProduct);
+    expect(commonFields.dateField.value).toBe(dateValue);
+    expect(commonFields.encargadoField.value).toBe(encargadaFer);
+    expect(commonFields.productField.value).not.toBe(testProduct);
   });
 
   test('renders difference value', async () => {
@@ -116,26 +121,56 @@ describe('record-creator.html', () => {
 
     const productField = document.getElementById('item-list');
     const currentStockField = document.getElementById('current-stock');
-    const newStock = document.getElementById('new-stock');
-    const differenceField = document.getElementById('difference');
-    const outputFields = document.getElementById('new-stock-output-fields');
-    const sotckOutType = document.getElementById('stock-output-type-list');
-    const stockOutputDesc = document.getElementById('new-output-desc');
+
+    const stockFields = getStockFields(document);
 
     const testProduct = 'Sal cocina';
     const currentStockInput = 5;
     const BAJA_VALUE = 'BAJA';
 
-    expect(stockOutputDesc).toBeDisabled();
+    expect(stockFields.stockOutputDesc).toBeDisabled();
 
     fireEvent.change(productField, { target: { value: testProduct } });
     expect(currentStockField.value).toBe('10');
 
-    fireEvent.input(newStock, { target: { value: currentStockInput } });
-    expect(differenceField.value).toBe('-5');
-    expect(outputFields).not.toBeDisabled();
+    fireEvent.input(stockFields.newStock, { target: { value: currentStockInput } });
+    expect(stockFields.differenceField.value).toBe('-5');
+    expect(stockFields.outputFields).not.toBeDisabled();
 
-    fireEvent.input(sotckOutType, { target: { value: BAJA_VALUE } });
-    expect(stockOutputDesc).not.toBeDisabled();
+    fireEvent.input(stockFields.sotckOutType, { target: { value: BAJA_VALUE } });
+    expect(stockFields.stockOutputDesc).not.toBeDisabled();
+  });
+
+  test("renders disabled fields while product isn't has value", async () => {
+    let { document } = await load(testPath);
+
+    const newStockTab = document.getElementById('new-stock-tab');
+    const inputTab = document.getElementById('input-tab');
+    const outputTab = document.getElementById('output-tab');
+
+    const stockFields = getStockFields(document);
+    const inputFields = getInputFields(document);
+    const outputFields = getOutputFields(document);
+
+    fireEvent.click(inputTab);
+
+    expect(inputFields.invoice).toBeDisabled();
+    expect(inputFields.amount).toBeDisabled();
+    expect(inputFields.quantity).toBeDisabled();
+    expect(inputFields.finalStockInput).toBeDisabled();
+
+    fireEvent.click(outputTab);
+
+    expect(outputFields.outQuantity).toBeDisabled();
+    expect(outputFields.finalStockOut).toBeDisabled();
+    expect(outputFields.outputTypeList).toBeDisabled();
+    expect(outputFields.outputDescription).toBeDisabled();
+
+    fireEvent.click(newStockTab);
+
+    expect(stockFields.newStock).toBeDisabled();
+    expect(stockFields.differenceField).toBeDisabled();
+    expect(stockFields.outputFields).not.toBeVisible();
+    expect(stockFields.inputFields).not.toBeVisible();
   });
 });
