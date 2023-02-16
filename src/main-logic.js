a3cde4341e1ef2baa02805be9fd5609670a22afc
Andres Felipe Torres <andrestor2@gmail.com>
2023-02-16 06:47:46 -0500

    feat/appDesign: Added logic to handle different pages (hand mande Router system).

--- src/main-logic.js
+++ src/main-logic.js
@@ -26,10 +26,72 @@ function activeNavbarLink(id) {
 }
 
 function getPageContent(pageId) {
-  google.script.run.withSuccessHandler(putHTMLContent).getHTMLPageContent(pageId);
+  if ('undefined' !== typeof google) {
+    google.script.run
+      .withSuccessHandler(putHTMLContent)
+      .withFailureHandler(failResponse)
+      .getHTMLPageContent(pageId);
+    return;
+  }
+}
+
+function failResponse() {
+  //redirect to home
+  //infinite loop
+  //getPageContent(HOME_LINK_ID);
 }
 
 function putHTMLContent(HTMLContent) {
   console.log(HTMLContent);
-  document.getElementById('main-content').innerHTML = HTMLContent;
+  document.getElementById('main-content').innerHTML = HTMLContent.content;
+  loadPageEventsByName(HTMLContent.pageName);
 }
+
+function loadPageEventsByName(pageName) {
+  console.log('pageName: ' + pageName);
+
+  switch (pageName) {
+    case HOME_LINK_ID:
+      loadHomePage();
+      break;
+    case INVENTORY_PAGE_ID:
+      loadRecordFormLogic();
+      break;
+    case CONFIGURATION_LINK_ID:
+      loadConfigurationPage();
+      break;
+    case REPORT_PAGE_ID:
+      loadReportPage();
+      break;
+  }
+}
+
+function loadRecordFormLogic() {
+  /* Global events */
+  startUpForm();
+  document.getElementById('clear-fields').addEventListener('click', clearFields);
+  document.getElementById('form-inventory').addEventListener('submit', dataValidation);
+
+  /* Tab Panel events */
+  document.getElementById(NEW_STOCK_TAB_ID).addEventListener('click', clickOnNewStock);
+  document.getElementById(INPUT_TAB_ID).addEventListener('click', clickOnInput);
+  document.getElementById(OUTPUT_TAB_ID).addEventListener('click', clickOnOutput);
+
+  /* New Stock events */
+  document.getElementById(ITEM_LIST_ID).addEventListener('change', getCurrentStock);
+  document.getElementById(NEW_STOCK_ID).addEventListener('input', processNewStock);
+  document.getElementById(STOCK_OUTPUT_TYPE_LIST).addEventListener('input', processStockOutputType);
+
+  /* Input events */
+  document.getElementById(QUANTITY_ID).addEventListener('input', processInQuantity);
+
+  /*Output event */
+  document.getElementById(OUT_QUANTITY_ID).addEventListener('input', processOutQuantity);
+  document.getElementById(OUTPUT_TYPE_LIST).addEventListener('input', processOutputTypeList);
+}
+
+function loadConfigurationPage() {}
+
+function loadReportPage() {}
+
+function loadHomePage() {}
