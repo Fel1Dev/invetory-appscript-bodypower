c531c85bee6c633f2345531c9c9504e2a6301954
Andres Felipe Torres <andrestor2@gmail.com>
2023-02-19 11:53:24 -0500

    feat/appDesign: Added details to new report feature. Interaction and styles improved.

--- src/report/report-logic.js
+++ src/report/report-logic.js
@@ -20,29 +20,17 @@ function getReportData() {
 }
 
 function addContentToTable(dataFromInventory) {
-  console.log(dataFromInventory);
-  const filteredData = processResponse(dataFromInventory);
   renderDataOnTable(dataFromInventory);
   stopLoadingScreen();
 }
 
-function processResponse(inventoryData) {
-  return filterLowerThanMinimal(inventoryData);
-}
-
-function filterLowerThanMinimal(inventoryData) {
-  console.log();
-  return inventoryData.map((row) => {
-    return row;
-  });
-}
-
 function renderDataOnTable(filteredData) {
+  console.log(filteredData);
   const tableBody = document.getElementById(TABLE_BODY_ID);
-
+  tableBody.textContent = '';
   let counter = 1;
+
   filteredData.forEach((row) => {
-    console.log(row);
     if (!row[1]) return;
 
     const tableRow = document.createElement('tr');
@@ -53,34 +41,25 @@ function renderDataOnTable(filteredData) {
     cellHeader.appendChild(cellHText);
     cellHeader.setAttribute('scope', 'row');
 
-    const nameCell = document.createElement('td');
-    const minStockCell = document.createElement('td');
-    const currStockCell = document.createElement('td');
-
-    const nameTextNode = document.createTextNode(row[1]);
-    const minTextNode = document.createTextNode(row[8]);
-    const currTextNode = document.createTextNode(row[7]);
-
-    nameCell.appendChild(nameTextNode);
-    minStockCell.appendChild(minTextNode);
-    currStockCell.appendChild(currTextNode);
-
-    console.log(nameCell);
-    console.log(minStockCell);
-    console.log(currStockCell);
-
-    console.log(tableRow);
+    const nameCell = createCell(row[1]);
+    const minStockCell = createCell(row[8]);
+    const currStockCell = createCell(row[7]);
 
     tableRow.appendChild(cellHeader);
     tableRow.appendChild(nameCell);
     tableRow.appendChild(minStockCell);
     tableRow.appendChild(currStockCell);
 
-    console.log(tableBody);
-
     tableBody.appendChild(tableRow);
     counter++;
   });
+}
+
+function createCell(value) {
+  const nameCell = document.createElement('td');
+  const nameTextNode = document.createTextNode(value);
+  nameCell.appendChild(nameTextNode);
+  return nameCell;
 }
 
 function failResponse() {
